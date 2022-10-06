81737a8092c3c972eb881d2d4203dbec063b8ad9
Miguel Machado <93016833+miguelmachadofs@users.noreply.github.com>
2022-10-06 10:43:26 -0300

    Initial commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers;
+
+    const listUsers = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.status(200).json(listUsers);
+  }
+}
+
+export { ListAllUsersController };
