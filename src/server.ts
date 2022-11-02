3cc1bd8af6f5d492d254867b5ea2ae83cff6d82d
Miguel Machado <93016833+miguelmachadofs@users.noreply.github.com>
2022-11-02 17:42:03 -0300

    Ajustes de configuração necessários

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
-import { app } from ".";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerFile from "./swagger.json";
+
+const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
